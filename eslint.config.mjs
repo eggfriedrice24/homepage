c2224181ce78455b6597d2162ce5be7adcbad28d
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-01-04 09:34:46 +0000

    chore: configure antfu eslint with next.js rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,29 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import antfu from "@antfu/eslint-config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
-
-export default eslintConfig;
+export default antfu({
+  type: "app",
+  typescript: true,
+  formatters: true,
+  next: true,
+  stylistic: {
+    indent: 2,
+    semi: true,
+    quotes: "double",
+  },
+}, {
+  rules: {
+    "ts/no-redeclare": "off",
+    "ts/consistent-type-definitions": ["error", "type"],
+    "no-console": ["warn"],
+    "antfu/no-top-level-await": ["off"],
+    "node/prefer-global/process": ["off"],
+    "node/no-process-env": ["error"],
+    "perfectionist/sort-imports": ["error", {
+      tsconfigRootDir: ".",
+    }],
+    "unicorn/filename-case": ["error", {
+      case: "kebabCase",
+      ignore: ["README.md"],
+    }],
+  },
+});
