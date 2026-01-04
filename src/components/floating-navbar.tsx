ca64cc5c19ae4feb99a6d9685ad700370518ebd5
eggfriedrice <eggfriedricew.g.o@gmail.com>
2026-01-04 10:11:59 +0000

    feat: add animated background, update floating navbar

--- src/components/floating-navbar.tsx
+++ src/components/floating-navbar.tsx
@@ -12,150 +12,116 @@ import {
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-import { Button, buttonVariants } from "@/components/ui/button";
+import { ThemeToggle } from "@/components/theme-toggle";
+import { AnimatedBackground } from "@/components/ui/animated-background";
+import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
-import { cn } from "@/lib/utils";
 
-import { ThemeToggle } from "./theme-toggle";
+const TABS = [
+  {
+    label: "Home",
+    href: "/",
+    icon: <Home className="size-5" />,
+  },
+  {
+    label: "Projects",
+    href: "/projects",
+    icon: <FolderKanban className="size-5" />,
+  },
+  {
+    label: "Lab",
+    href: "/lab",
+    icon: <FlaskConical className="size-5 text-emerald-500" />,
+  },
+];
+
+const SOCIALS = [
+  {
+    label: "GitHub",
+    href: "https://github.com/eggfriedrice24",
+    icon: <Github className="size-5" />,
+  },
+  {
+    label: "LinkedIn",
+    href: "https://www.linkedin.com/in/irakli-dzvelaia",
+    icon: <Linkedin className="size-5 text-sky-700" />,
+  },
+  {
+    label: "Twitter",
+    href: "https://twitter.com/IDzvelaia",
+    icon: <Twitter className="size-5 text-sky-400" />,
+  },
+];
 
 export function FloatingNavbar() {
   const pathname = usePathname();
 
-  const isActiveRoute = (route: string) => {
-    if (route === "/") {
-      return pathname === route;
-    }
-    return pathname.startsWith(route);
+  const getActiveTab = () => {
+    if (pathname === "/")
+      return "Home";
+    if (pathname.startsWith("/projects"))
+      return "Projects";
+    if (pathname.startsWith("/lab"))
+      return "Lab";
+    return "Home";
   };
 
   return (
     <nav className="pointer-events-none fixed inset-x-0 bottom-0 z-20 mx-auto mb-4 flex h-12 px-6">
-      <div className="pointer-events-auto relative mx-auto flex h-full items-center gap-2 overflow-y-scroll rounded-2xl bg-background/10 px-4 py-2 shadow-[rgba(142,140,152,0.2)_0px_0px_30px,rgba(219,216,224,0.2)_0px_0px_0px_1px] backdrop-blur-md sm:overflow-y-visible">
-        <div className="flex items-center gap-2">
-          <TooltipProvider delayDuration={0}>
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="/"
-                  className={cn(
-                    buttonVariants({ variant: "ghost", size: "icon" }),
-                    isActiveRoute("/") && "bg-accent text-accent-foreground",
-                  )}
-                >
-                  <Home className="size-5" />
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-secondary text-foreground">
-                <p>Home</p>
-              </TooltipContent>
-            </Tooltip>
+      <div className="pointer-events-auto relative mx-auto flex h-full items-center gap-2 rounded-xl border border-zinc-950/10 bg-white p-2 shadow-lg dark:border-zinc-50/10 dark:bg-zinc-900">
+        <AnimatedBackground
+          defaultValue={getActiveTab()}
+          className="rounded-lg bg-zinc-100 dark:bg-zinc-800"
+          transition={{
+            type: "spring",
+            bounce: 0.2,
+            duration: 0.3,
+          }}
+        >
+          {TABS.map(tab => (
+            <Link
+              key={tab.label}
+              href={tab.href}
+              data-id={tab.label}
+              className="inline-flex size-9 items-center justify-center text-zinc-500 transition-colors duration-100 focus-visible:outline-2 data-[checked=true]:text-zinc-950 dark:text-zinc-400 dark:data-[checked=true]:text-zinc-50"
+            >
+              {tab.icon}
+            </Link>
+          ))}
+        </AnimatedBackground>
 
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="/projects"
-                  className={cn(
-                    buttonVariants({ variant: "ghost", size: "icon" }),
-                    isActiveRoute("/projects")
-                    && "bg-accent text-accent-foreground",
-                  )}
-                >
-                  <FolderKanban className="size-5" />
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-secondary text-foreground">
-                <p>Projects</p>
-              </TooltipContent>
-            </Tooltip>
+        <Separator orientation="vertical" className="mx-1 h-6" />
 
-            <Tooltip>
-              <TooltipTrigger asChild>
-                <Link
-                  href="/lab"
-                  className={cn(
-                    buttonVariants({ variant: "ghost", size: "icon" }),
-                    isActiveRoute("/lab")
-                    && "bg-accent text-accent-foreground",
-                  )}
-                >
-                  <FlaskConical className="size-5 text-emerald-500" />
-                </Link>
-              </TooltipTrigger>
-              <TooltipContent className="bg-secondary text-foreground">
-                <p>Lab</p>
-              </TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-        </div>
-
-        <Separator className="mx-3 h-6" orientation="vertical" />
-
-        <div className="flex items-center gap-2">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size="icon">
-                <MoreHorizontal className="size-5" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent className="w-56">
-              <DropdownMenuGroup>
-                <DropdownMenuItem>What I use</DropdownMenuItem>
-              </DropdownMenuGroup>
-              <DropdownMenuSeparator />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" size="icon" className="size-9">
+              <MoreHorizontal className="size-5" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="center">
+            {SOCIALS.map(social => (
               <a
-                rel="noreferrer"
+                key={social.label}
+                href={social.href}
                 target="_blank"
-                href="https://github.com/eggfriedrice24"
-                className="transition-all hover:scale-105"
+                rel="noreferrer"
               >
-                <DropdownMenuItem>
-                  <Github className="size-5" />
-                  GitHub
+                <DropdownMenuItem className="cursor-pointer gap-2">
+                  {social.icon}
+                  {social.label}
                 </DropdownMenuItem>
               </a>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
 
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://www.linkedin.com/in/irakli-dzvelaia"
-                className="transition-all hover:scale-105"
-              >
-                <DropdownMenuItem>
-                  <Linkedin className="size-5 text-sky-700" />
-                  LinkedIn
-                </DropdownMenuItem>
-              </a>
-
-              <a
-                rel="noreferrer"
-                target="_blank"
-                href="https://twitter.com/IDzvelaia"
-                className="transition-all hover:scale-105"
-              >
-                <DropdownMenuItem>
-                  <Twitter className="size-5 text-sky-400" />
-                  Twitter
-                </DropdownMenuItem>
-              </a>
-            </DropdownMenuContent>
-          </DropdownMenu>
-
-          <ThemeToggle />
-        </div>
+        <ThemeToggle />
       </div>
     </nav>
   );
